34196c3282d0b6935714f755f632f717ef6116c0
ivan <ivan.portillo@mo2o.com>
2025-07-22 15:28:22 +0200

    initial project with Herd + php

---
+++ contact.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0        
+">
+    <title>My Web Page</title>
+    <link rel="stylesheet" href="styles.css">
+</head> 
+<body>
+    <header>
+        <h1>Contact with me</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/post.php">Post</a></li>
+            <li><a href="/about.php">About</a></li>
+            <li><a href="/contact.php">Contact</a></li>
+            <li><a href="/links.php">Proyectos</a></li>
+        </ul>
+    </nav>
+    <main>
+        <h2>Content Section</h2>
+        <p>Contact with me</p>
+    </main>
+    <footer>
+        <p>&copy; 2025 My Web Page</p>
+    </footer>
+    <script src="script.js"></script>
+</body>
+</html>
+<?php
+// index.php - Main entry point forthe web application
---
+++ setup.md
@@ -0,0 +1,2 @@
+# Tools
+- [ Laravel Herd](https://herd.laravel.com/download/latest/macos)
